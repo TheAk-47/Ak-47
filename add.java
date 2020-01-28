67dbf5dbb73a20320b4283d11446c3eccc495926
TheAk-47 <n.ajay073@gmail.com>
2020-01-28 14:24:28 +0530

    java

---
+++ add.java
@@ -0,0 +1,9 @@
+class add{
+	public static void main(String[] args){
+		int a,b,c;
+		a=20;
+		b=30;
+		c=50;
+		System.out.print(a+b+c);
+	}
+}
---
+++ sum.java
@@ -0,0 +1,12 @@
+class sum
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		a=50;
+		b=30;
+		c=120;
+		c=a+b+c;
+		system.out.println("the sum of three values");
+	}
+}
